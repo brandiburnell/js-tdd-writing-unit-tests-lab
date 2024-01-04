f4bc12cc5d404484c3fc8aa07497a8725123b947
Brandi Burnell <brandiburnell6@gmail.com>
2024-01-03 20:09:39 -0700

    write and pass all tests

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,26 @@
 // Your tests here
+import { isPallindrome } from "../utils"; 
+
+describe("isPallindrome", () => {
+    it("handles uppercase and lowercase input", () => {
+        const word = "rAcEcAr";
+
+        const result = isPallindrome(word);
+
+        expect(result).toBe(true);
+    });
+    it("returns false for an empty string", () => {
+        const emptyString = "";
+
+        const result = isPallindrome(emptyString);
+
+        expect(result).toBe(false);
+    });
+    it("returns true if word is a pallindrome", () => {
+        const word = "car";
+
+        const result = isPallindrome(word);
+
+        expect(result).toBe(false);
+    });
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,9 @@
 // Your code here
+export function isPallindrome(str) {
+    if (str === "") {
+        return false;
+    }
+    else {
+        return str.toLowerCase() == str.toLowerCase().split('').reverse().join('');
+    }
+}
